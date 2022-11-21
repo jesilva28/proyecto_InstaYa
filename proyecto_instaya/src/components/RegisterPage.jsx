6739c2ed1a5c224bcf6bc422f86cc9086849e23f
Johanna1099 <lcalderonj@uninorte.edu.co>
2022-11-20 22:36:44 -0500

    registerpage

---
+++ proyecto_instaya/src/components/RegisterPage.jsx
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../src/CSS/paginaregistro.css"/>
+    <title>Iniciar sesión</title>
+</head>
+<body>
+    <h1>InstaYA - MENSAJERÍA</h1>
+    <div class="front-page">
+        <div class="register">
+            <h2>REGISTRO DE USUARIOS</h2>
+            <h3>Nombres: <input></h3>
+            <h3>Usuario: <input></h3>
+            <h3>Contraseña: <input></h3>
+            <h3>Correo: <input></h3>
+            
+            <button type="submit">Crear</button><br>
+            
+        </div>
+       
+    </div>
+    
+    
+</body>
+</html>

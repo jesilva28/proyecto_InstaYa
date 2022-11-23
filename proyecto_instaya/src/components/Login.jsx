4fe871a3eef362d60f59c911da990b4187b02358
jesilva28 <jesilva@uninorte.edu.co>
2022-11-23 00:22:52 -0500

    update handleChange and handleSubmit

--- proyecto_instaya/src/components/Login.jsx
+++ proyecto_instaya/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import box from '../static/box.png'
 
 export function Login() {
@@ -6,13 +6,27 @@ export function Login() {
     // useEffect(() => {
     //     document.title = "Inicio de sesión";
     // }, []);
+
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]:value}));
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputs);
+    }
+
     return (
         <div className="d-flex flex-column align-items-center">
             <h1 className="text-center mt-3 mb-5" style={{color: "white"}}>InstaYA - MENSAJERÍA</h1>
             <div className="d-flex flex-column align-items-center rounded w-50 shadow" style={{backgroundColor: "white"}}>
                 <h3 className="py-5">INICIO DE SESIÓN</h3>
                 <img src={box} alt="logo" style={{width:"15%"}}></img>
-                <form className="d-flex flex-column align-items-center">
+                <form className="d-flex flex-column align-items-center" onSubmit={handleSubmit}>
                     <div>
                         <div className="my-4 row">
                             <label for="username" className="mt-1 form-label col">Usuario:</label>
@@ -22,7 +36,9 @@ export function Login() {
                                     className="form-control rounded col" 
                                     id="username" 
                                     placeholder="Ingrese su id." 
-                                    name="username">
+                                    name="username"
+                                    value={inputs.username || ""}
+                                    onChange={handleChange}>
                                 </input>
                             </div>
                         </div>
@@ -34,7 +50,9 @@ export function Login() {
                                     className="form-control rounded col" 
                                     id="pwd" 
                                     placeholder="Ingrese su contraseña" 
-                                    name="pwd">
+                                    name="pwd"
+                                    value={inputs.pwd || ""}
+                                    onChange={handleChange}>
                                 </input>
                             </div>
                         </div>
